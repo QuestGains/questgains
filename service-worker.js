b02e74774cc1ef75094fc3e6ec2771f97447d635
Alfred <alfred@questgains.app>
2026-05-03 14:12:33 -0400

    SW v18

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
  * QuestGains service worker
  * Cache-first app shell and sprite assets for offline support.
  */
-const CACHE_NAME = 'questgains-v17';
+const CACHE_NAME = 'questgains-v18';
 const PRECACHE_URLS = [
   './index.html',
   './firebase-config.js',
